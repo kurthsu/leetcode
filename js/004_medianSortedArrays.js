c6c3792c87abc07be357975dcd0619cba20dbcdc
Kurt Hsu <kurt@picowork.com>
2018-03-16 07:04:57 +0800

    Add JS folder

---
+++ js/004_medianSortedArrays.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+  
+};
+
+var assert = require('assert');
+describe('#findMedianSortedArrays()', function() {
+  it('should return -1 when the value is not present', function() {
+    assert.equal([1,2,3].indexOf(4), -1);
+  });
+});
